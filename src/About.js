01244a1acba50b54d369120a2bf3cb42d26c8d69
sharmajiiprince <prince.raj.cs19@ggits.net>
2024-03-07 14:45:00 +0530

    my clone

---
+++ src/About.js
@@ -0,0 +1,7 @@
+export default function About(){
+    return (
+        <div>
+            <h3>This is my About page !</h3>
+        </div>
+    )
+}
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+export default function Home(){
+    return (
+        <div>
+            <h3>This is my Home page !</h3>
+        </div>
+    )
+}
